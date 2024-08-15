295d3c39fca405274ae20253baf0e6be7e9c4697
travon martin <martitra000@warren.k12.in.us>
2024-08-15 08:41:55 -0400

    75

---
+++ var1.py
@@ -0,0 +1,16 @@
+Num_apples = 20
+
+Num_oranges = 15
+
+print("Number of apples:", Num_apples)
+
+print("Number of oranges", Num_oranges)
+
+Num_apples = 0
+
+Num_oranges = 0
+
+print("Number od apples:", Num_apples)
+
+print("Number of oranges", Num_oranges)
+
